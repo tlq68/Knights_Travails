8b205b6da22c9cc8cd778999fad634d939c2721d
Trevor <tlist403@gmail.com>
2021-11-16 12:41:07 -0300

    Adjusted Square and Board attributes

--- knights_travails.rb
+++ knights_travails.rb
@@ -11,6 +11,12 @@ class Knight
 end
 
 class Board
+    attr_accessor :coordinate_set
+
+    def initialize()
+        @coordinate_set = connect_squares(create_board)
+    end
+
     def create_board(size = 8)
         # With this, we can just change the array input to be inner and that will give us a different setup. But this way may be best for now. Using the coordinates with no intermediate step.
         array = []
@@ -21,23 +27,46 @@ class Board
             end
             array.push(inner_array)
         end
-        p array
+        array
     end
+
+    def connect_squares(array_of_coordinates)
+        board = array_of_coordinates
+        board_with_squares = board.each do |outer|
+            outer.each do |inner|
+                inner = Square.new(inner)
+            end
+        end
+
+        board_with_squares
+    end
+
+    
+
 end
 
 class Square 
-    attr_accessor :coordinates, :top, :right, :bottom, :left
+    # The starting position was chosen as the topmost right space that the knight could move and then the other attributes continue clockwise
+    attr_accessor :coordinates, :up2_right1, :up1_right2, :down1_right2, :down2_right1, :down2_left1, :down1_left2, :up1_left2, :up2_left1
 
-    def initialize(coordinates, top = nil, right = nil, bottom = nil, left = nil)
+    def initialize(coordinates, up2_right1 = nil, up1_right2 = nil, down1_right2 = nil, down2_right1 = nil, down2_left1 = nil, down1_left2 = nil, up1_left2 = nil, up2_left1 = nil)
         @coordinates = coordinates
     end
 end
 
-test = Board.new.create_board
+
+#p Board.new.set_coordinates 
 
 
-test.each do |outer|
-    outer.each do |inner|
-        p inner
-    end
-end
+
+
+test = Board.new
+
+p test
+
+
+# Next step is to assign a coordinate to each square. Then we should try to assign all of the children nodes that can be accessed by a particular square. This should be done in a way that coincides with the MOVEMENT OF THE KNIGHT. So, any square could be connected to up to 8 other squares.
+
+# After that, we should find a way to traverse the nodes so that we can see what is happening. We will do this in a standard way. 
+
+# Finally, we will try to find a way to traverse with the knight movements and then we will try to go from point A to point B.
